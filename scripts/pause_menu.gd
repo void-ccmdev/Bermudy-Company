ebbe41c16df569881be5b4c91f6000e7f9b06e95
ccmdev <controlcommanddev@gmail.com>
2025-12-30 20:19:17 +0100

    Added an UI theme and setup a base for a PAUSE MENU

---
+++ scripts/pause_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_play_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_main_menu_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	pass # Replace with function body.
